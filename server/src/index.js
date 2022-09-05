a9b0136b7dbdff72d892868ea54f1d327572a332
Santhosh SS <santhoshss@Santhoshs-MacBook-Pro-2.local>
2022-09-05 17:52:05 +0100

    update port and url

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,9 @@ const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(() => {
-  console.log(`Server is running!
-    Listening on port 4000 
-    Explore at https://studio.apollographql.com/sandbox
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`
+    🚀  Server is ready at ${url}
+    📭  Query at https://studio.apollographql.com/dev
   `);
 });
